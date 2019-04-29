abd0c9874050084595ec2cf02943075cf639958f
Colin <why101@msn.com>
2019-04-29 15:48:36 -0700

    added wbRArrayS function converter

---
+++ src/Converter/functions/wbRArrayS.js
@@ -0,0 +1,10 @@
+let {addRequires, functionConverter} = require('../../converter'),
+    {indent, args} = require('../../helpers');
+
+functionConverter('wbRArrayS', [
+    args.name,
+    { type: 'function', id: 'wbStructSK', name: 'struct' }
+], ({name, struct}) => {
+    addRequires('arrayOfSubrecord');
+    return `arrayOfSubrecord('${name}', ${indent(struct)})`;
+});
