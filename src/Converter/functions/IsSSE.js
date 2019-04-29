6b0ccd1f2e3853af9c80053e191e1a6d472f1d9e
Colin <why101@msn.com>
2019-04-29 15:47:24 -0700

    added IsSSE function converter

---
+++ src/Converter/functions/IsSSE.js
@@ -0,0 +1,10 @@
+let {addRequires, functionConverter} = require('../../converter'),
+    {arr} = require('../../helpers');
+
+functionConverter('IsSSE', [
+    { type: 'member', name: 'm1' },
+    { type: 'member', name: 'm2' }
+], ({m1, m2}) => {
+    addRequires('IsSSE');
+    return `IsSSE(game, ${arr([m1, m2])})`;
+});
