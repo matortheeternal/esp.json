3eba8bb1d8ec99745c70ec4742903b0320e41a41
Colin <why101@msn.com>
2019-04-29 15:47:52 -0700

    added wbFlags function converter

---
+++ src/Converter/functions/wbFlags.js
@@ -0,0 +1,15 @@
+let {functionConverter} = require('../../converter'),
+    {args} = require('../../helpers');
+
+let convertFlags = ({flags}) => {
+    return JSON.stringify(flags, null, 2);
+};
+
+// inherit flags
+functionConverter('wbFlags', [
+    args.identifier,
+    { type: 'function', id: 'wbFlagsList', name: 'flags' }
+], convertFlags);
+
+// new flags
+functionConverter('wbFlags', [args.flags], convertFlags);
