0503f35dd0af1db088445f4cd4a0647b3e73c0bf
Colin <why101@msn.com>
2019-04-29 15:50:38 -0700

    added flags type converter

---
+++ src/Converter/types/flags.js
@@ -0,0 +1,6 @@
+let {typeConverter} = require('../../converter');
+
+typeConverter('flags', {
+    test: context => context.matchArray('string'),
+    convert: match => Object.assign({}, match.entries)
+});
