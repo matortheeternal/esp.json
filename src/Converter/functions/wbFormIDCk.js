9d9cffafa3c1731f84a4a80a1eabf5c61a6688c6
Colin <why101@msn.com>
2019-04-29 15:48:12 -0700

    added wbFormIDCk function converter

---
+++ src/Converter/functions/wbFormIDCk.js
@@ -0,0 +1,15 @@
+let {addRequires, functionConverter} = require('../../converter'),
+    {reqLine, args} = require('../../helpers');
+
+functionConverter('wbFormIDCk', [
+    args.sig,
+    args.name,
+    args.signatures,
+    args.boolean,
+    args.identifier,
+    args.required
+], ({sig, name, signatures, required}) => {
+    addRequires('subrecord', 'ckFormId');
+    return reqLine(required, `subrecord('${sig}', ckFormId(` +
+        `'${name}', ${signatures}))`);
+});
