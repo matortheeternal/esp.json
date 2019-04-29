72f95bf924c5329cd82f42f9629143fe467df6af
Colin <why101@msn.com>
2019-04-29 15:51:03 -0700

    added signature type converter

---
+++ src/Converter/types/signature.js
@@ -0,0 +1,8 @@
+let {typeConverter} = require('../../converter');
+
+let sigExpr = /^([A-Z]{1}[A-Z0-9]{3})/;
+
+typeConverter('signature', {
+    test: context => context.match(sigExpr),
+    convert: match => match[1]
+});
