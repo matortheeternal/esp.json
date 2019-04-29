c8dc305d9533abf24b6e44a8c3ef3fce179d684f
Colin <why101@msn.com>
2019-04-29 15:47:41 -0700

    added wbEmpty function converter

---
+++ src/Converter/functions/wbEmpty.js
@@ -0,0 +1,12 @@
+let {addRequires, functionConverter} = require('../../converter'),
+    {reqLine, args} = require('../../helpers');
+
+functionConverter('wbEmpty', [
+    args.sig,
+    args.name,
+    args.identifier,
+    args.required,
+], ({sig, name, required}) => {
+    addRequires('subrecord', 'empty');
+    return reqLine(required, `subrecord('${sig}', empty('${name}'))`);
+});
