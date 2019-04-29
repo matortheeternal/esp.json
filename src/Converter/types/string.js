8e792cdbc10258877e6f1ff4fdc32da737a07aa0
Colin <why101@msn.com>
2019-04-29 15:51:08 -0700

    added string type converter

---
+++ src/Converter/types/string.js
@@ -0,0 +1,8 @@
+let {typeConverter} = require('../../converter');
+
+let strExpr = /^'((?:[^']+|'')*)'/;
+
+typeConverter('string', {
+    test: context => context.match(strExpr),
+    convert: match => match[1].replace(/''/g, '\'')
+});
