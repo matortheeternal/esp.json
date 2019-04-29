5567a161666c3da80c6183ac6553ecf2b7e472cb
Colin <why101@msn.com>
2019-04-29 15:50:13 -0700

    added array of member type converter

---
+++ src/Converter/types/arrayOfMember.js
@@ -0,0 +1,6 @@
+let {typeConverter} = require('../../converter');
+
+typeConverter('array of member', {
+    test: context => context.matchArray('identifier', 'function'),
+    convert: match => match.value
+});
