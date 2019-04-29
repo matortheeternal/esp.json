b715a0da8cbb39d357b3713a75c91fa6bb912be5
Colin <why101@msn.com>
2019-04-29 15:48:41 -0700

    added wbRecord function converter

---
+++ src/Converter/functions/wbRecord.js
@@ -0,0 +1,21 @@
+let {addRequires, functionConverter} = require('../../converter'),
+    {obj, arr, args} = require('../../helpers');
+
+functionConverter('wbRecord', [
+    args.sig,
+    args.name,
+    { type: 'function', id: 'wbFlags', name: 'flags' },
+    args.members,
+    args.boolean,
+    args.identifier,
+    args.identifier,
+    args.boolean,
+    args.identifier,
+    args.identifier
+], ({sig, name, flags, members}) => {
+    addRequires('record');
+    return `record('${sig}', '${name}', ` + obj([
+        `flags: ${flags}`,
+        `members: ${arr(members)}`
+    ]);
+});
