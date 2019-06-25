79ba24d417a035eb03a0fdd5d1c5f548f6e7a320
Colin <why101@msn.com>
2019-06-24 18:32:20 -0700

    wbFlagsList now sets flags like xEdit does

--- src/Converter/functions/wbFlagsList.js
+++ src/Converter/functions/wbFlagsList.js
@@ -1,7 +1,21 @@
 let {functionConverter} = require('../converters'),
     {args} = require('../helpers');
 
+let setUnknowns = function(flags) {
+    for (let i = 0; i < 32; i++) {
+        if (flags.hasOwnProperty(i)) continue;
+        flags[i] = `Unknown ${i}`;
+    }
+};
+
 functionConverter('wbFlagsList', [
     args.enum,
-    args.boolean
-], args => args.options);
+    { type: 'boolean', name: 'deleted' },
+    { type: 'boolean', name: 'unknowns' }
+], args => {
+    let flags = args.options;
+    if (args.deleted) flags[5] = 'Deleted';
+    flags[12] = 'Ignored';
+    if (args.unknowns) setUnknowns(flags);
+    return flags;
+});
