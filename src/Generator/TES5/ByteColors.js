553eb3cc9c6beb8e9c17498bdd9f234076b5ff1d
Colin <why101@msn.com>
2019-06-26 10:16:54 -0700

    added color def functions to generator

---
+++ src/Generator/TES5/ByteColors.js
@@ -0,0 +1,14 @@
+let {
+    addDef, struct, bytes, uint8, size
+} = require('../helpers');
+
+module.exports = () => {
+    addDef('ByteColors',
+        struct('', [
+            uint8('Red'),
+            uint8('Green'),
+            uint8('Blue'),
+            size(1, bytes('Unused'))
+        ])
+    );
+};
