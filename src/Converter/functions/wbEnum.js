ed350f6879dc286af8eaadfbd72ccff5358f8074
Colin <why101@msn.com>
2019-06-26 11:01:47 -0700

    added wbEnum function converter for local variable argument

--- src/Converter/functions/wbEnum.js
+++ src/Converter/functions/wbEnum.js
@@ -9,3 +9,16 @@ functionConverter('wbEnum', [
     options = Object.assign({}, flags, options || {});
     return `enumeration(${stringify(options)})`;
 });
+
+functionConverter('wbEnum', [
+    args.flags,
+    args.identifier
+], (args, converter) => {
+    converter.addRequires('enumeration');
+    let options = Object.assign(
+        {},
+        args.flags,
+        converter.getData(args.values[1]) || {}
+    );
+    return `enumeration(${stringify(options)})`;
+});
