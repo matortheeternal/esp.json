46f64eb355feb4cbd3c23ad5f3c4471ee93c700d
Colin <why101@msn.com>
2019-06-26 11:00:49 -0700

    ifGameMode now sets converter.skipping

--- src/Converter/statements/ifGameMode.js
+++ src/Converter/statements/ifGameMode.js
@@ -5,9 +5,10 @@ let ifGameModeExpr = /^if wbGameMode = (\w+) then\s*(begin)?/;
 statementConverter('ifGameMode', {
     test: parser => parser.match(ifGameModeExpr),
     convert: (converter, match) => {
-        converter.gameMode = match[1];
+        converter.skipping = converter.gameMode !== match[1];
         let endExpr = match[2] ? /^end;?/ : /^;/;
         while (!converter.match(endExpr))
             convertStatement(converter);
+        converter.skipping = false;
     }
 });
