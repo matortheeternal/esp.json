95a27bca43261463a3607767da052745f38aaa4e
Colin <why101@msn.com>
2019-06-26 10:20:59 -0700

    added ifGameMode statement converter

---
+++ src/Converter/statements/ifGameMode.js
@@ -0,0 +1,13 @@
+let {statementConverter, convertStatement} = require('../converters');
+
+let ifGameModeExpr = /^if wbGameMode = (\w+) then\s*(begin)?/;
+
+statementConverter('ifGameMode', {
+    test: parser => parser.match(ifGameModeExpr),
+    convert: (converter, match) => {
+        converter.gameMode = match[1];
+        let endExpr = match[2] ? /^end;?/ : /^;/;
+        while (!converter.match(endExpr))
+            convertStatement(converter);
+    }
+});
