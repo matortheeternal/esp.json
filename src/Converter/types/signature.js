f9e8b67b36d3ccf33f2f4108c8f2620f32717647
Mator <why101@msn.com>
2020-08-20 23:41:06 -0700

    fixed WTHR signature parsing

--- src/Converter/types/signature.js
+++ src/Converter/types/signature.js
@@ -1,10 +1,12 @@
 let {typeParser} = require('../parsers');
 
 let sigExpr = /^[A-Z]{1}[A-Z0-9_]{3}/,
-    specialSigExpr = /^_([0-9A-F]{2})h?_([A-Z0-9_]{3})/;
+    specialSigExpr = /^_([0-9A-F]{2}h?)_([A-Z0-9_]{3})/;
 
 let parseSpecialSig = function(match) {
-    return `\\x${match[1]}${match[2]}`;
+    if (match[2] === '0TX' && match[1][1] === '0' && match[1].length === 2)
+        return `\\x3${match[1][0]}${match[2]}`;
+    return `\\x${match[1].slice(0, 2)}${match[2]}`;
 };
 
 typeParser('signature', {
