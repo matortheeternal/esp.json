4665db1536c86259ca335f838832564f84f554e9
Mator <why101@msn.com>
2022-01-28 16:06:14 -0800

    schema fixes and additions

--- tasks/buildSchema.js
+++ tasks/buildSchema.js
@@ -2,10 +2,21 @@ let schemaBuilder = require('../src/SchemaBuilder'),
     path = require('path'),
     fs = require('fs');
 
-let projectPath = path.resolve(__dirname, '..'),
-    filename = 'gameDefinitionFile.schema.json',
-    schemaPath = path.resolve(projectPath, 'schema', filename);
+let projectPath = path.resolve(__dirname, '..');
 
-console.log('Building', schemaPath);
-let builtSchema = schemaBuilder.build(schemaPath);
-fs.writeFileSync('schema.json', JSON.stringify(builtSchema, null, 4));
+let buildSchema = function(schemaPathParts, output) {
+    let schemaPath = path.resolve(...schemaPathParts);
+    console.log('Building', schemaPath);
+    let builtSchema = schemaBuilder.build(schemaPath);
+    fs.writeFileSync(output, JSON.stringify(builtSchema, null, 4));
+};
+
+buildSchema(
+    [projectPath, 'schema', 'gameDefinitionFile.schema.json'],
+    'schema.json'
+);
+
+buildSchema(
+    [projectPath, 'schema', 'definitionEntry.schema.json'],
+    'definitionEntrySchema.json'
+);
